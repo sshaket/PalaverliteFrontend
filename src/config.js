ae8a5096dedeecb77d9b6d2de2697d2074a3dac1
Shaket <61824162+sshaket@users.noreply.github.com>
2025-05-17 23:20:29 +0530

    Add Socket URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const config = {
-    BASE_URL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000'
+    BASE_URL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
+    SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'http://localhost:3000'
 };
 
 export default config;
